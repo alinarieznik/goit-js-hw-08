8dde409b91746bde9abb72e302dd2594316b5889
Alina Rieznik <alina.raicheva97@gmail.com>
2023-03-31 18:37:41 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,18 @@
-// import Player from '@vimeo/player';
-// console.log(Player);
+import Player from '@vimeo/player';
+console.log(Player);
 
-// const iframe = document.querySelector('#vimeo-player');
-// const player = new Player(iframe);
+import throttle from 'lodash.throttle';
+console.log(throttle);
 
-// player.on('timeupdate', onPlay);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
-// function onPlay() {
-//   duration: 61.857;
-//   percent: 0.049;
-//   seconds: 3.034;
-// }
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(e) {
+  localStorage.setItem(STORAGE_KEY, e.seconds);
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)));
