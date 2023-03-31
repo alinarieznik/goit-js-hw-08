c3124e69c05d1b2a94d839a5939e773f738b0f3e
Alina Rieznik <alina.raicheva97@gmail.com>
2023-03-31 17:11:01 +0300

    add feature task 1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+// import Player from '@vimeo/player';
+// console.log(Player);
 
+// const iframe = document.querySelector('#vimeo-player');
+// const player = new Player(iframe);
+
+// player.on('timeupdate', onPlay);
+
+// function onPlay() {
+//   duration: 61.857;
+//   percent: 0.049;
+//   seconds: 3.034;
+// }
